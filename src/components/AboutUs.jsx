2f9fc38ff861e20c0a64985cdd06a35929aac858
Chitranjan Gupta <chitranjangupta40@gmail.com>
2026-04-22 17:31:10 +0530

    Update AboutUs component content and structure

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -2,10 +2,15 @@ import React from "react";
 
 function AboutUs() {
   return (
-    <div>
+    <div className="about-us-container">
       <h1>About Paradise Nursery</h1>
+      
       <p>
-        Paradise Nursery offers a wide variety of indoor plants to decorate your home.
+        Paradise Nursery is a plant shop that offers a wide variety of indoor and outdoor plants.
+      </p>
+
+      <p>
+        Our mission is to provide high-quality plants and help customers create a green and healthy environment.
       </p>
     </div>
   );
